ee0a08b520f0a4de432019d4decf456b0d99fcdd
Patrick Bednarski <patrick.bednarski@yuvo.io>
2018-02-10 20:21:07 -0500

    modify gitignore to ignore NetBeans auto-generated files

---
+++ src/naturallysilly/NaturallySilly.java
@@ -0,0 +1,16 @@
+package naturallysilly;
+
+/**
+ *
+ * @author Patman1
+ */
+public class NaturallySilly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
